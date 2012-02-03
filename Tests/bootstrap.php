8f0b27cc8d396f4d5e4125123fe94d3a16044d0e
hichem <hichem@hichem-Deb.(none)>
2012-02-03 11:07:40 +0100

    include TravisCi

---
+++ Tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of the AliDatatableBundle from the FOSUserBundle package.
+ *
+ * @see FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ */
+
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
+
