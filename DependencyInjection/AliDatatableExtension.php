00663f9b32f8202ff54942732f004b8a6bc48a41
Benjamin Ugbene <benjamin.ugbene@kiddivouchers.com>
2012-12-10 12:59:16 +0000

    added Twig No Requirements Routing Extension

--- DependencyInjection/AliDatatableExtension.php
+++ DependencyInjection/AliDatatableExtension.php
@@ -24,5 +24,8 @@ class AliDatatableExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('routing.xml');
     }
 }
