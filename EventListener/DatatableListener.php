AliHichem/AliDatatableBundle

commit e9d414b4aa661e744cd41d66a4b4e7c59e77e3b5

Fix warning in case session data not exist
Warning: array_walk() expects parameter 1 to be array, null given.

EventListener/DatatableListener.php
@@ -68,10 +68,13 @@ class DatatableListener implements EventSubscriberInterface
         $pos_container = strripos($content, 'alidatatable-scripts');
         $sess_dta      = $session->get('datatable',array());
         $dta_script    = null;
-        array_walk($sess_dta, function(&$part, &$key) {
-            $part = trim(preg_replace('/\s\s+/', ' ', $part));
-        });
-        $dta_script = implode("\n", $sess_dta);
+        if ($sess_data)
+        {
+            array_walk($sess_dta, function(&$part, &$key) {
+                $part = trim(preg_replace('/\s\s+/', ' ', $part));
+            });
+            $dta_script = implode("\n", $sess_dta);
+        }
         $session->set('datatable', array());
         if (!$pos_container)
         {

commit 4660dfeb3c8a9704aa7c5422e6dbaef5af8bc0e5

Fix typo for DatatableListener
Fix typo introduced in e9d414b.

EventListener/DatatableListener.php
@@ -68,7 +68,7 @@ class DatatableListener implements EventSubscriberInterface
         $pos_container = strripos($content, 'alidatatable-scripts');
         $sess_dta      = $session->get('datatable',array());
         $dta_script    = null;
-        if ($sess_data)
+        if ($sess_dta)
         {
             array_walk($sess_dta, function(&$part, &$key) {
                 $part = trim(preg_replace('/\s\s+/', ' ', $part));